704bb2ecbb9d2052d731dab7fd3218253496e0d7
tatiana191 <tatiananozhenko041@gmail.com>
2022-01-17 23:07:20 +0200

    task 1

---
+++ task 1/main.js
@@ -0,0 +1,13 @@
+let array = [1,3,5,4,5,7];
+function compare() {
+    let result = [];
+    for (let i = 0; i < array.length - 2; i++) {
+        if (array[i] < array[i + 1] && array[i + 1] < array[i + 2]) {
+            result +=" 1"
+        } else {
+            result += " 0"
+        }
+        document.querySelector('.output').innerHTML = result;
+    }
+}
+document.querySelector('.button').onclick = compare;
